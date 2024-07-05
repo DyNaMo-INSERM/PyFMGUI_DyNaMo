244feb92fedcb1d5762504ee9bac4ee546caaa2c
yogi-boi <136353187+yogi-boi@users.noreply.github.com>
2024-07-05 14:27:30 +0200

    including park in the gui

--- src/pyfmgui/main_window.py
+++ src/pyfmgui/main_window.py
@@ -206,6 +206,8 @@ class MainWindow(QtWidgets.QMainWindow):
 				"""
 				JPK files (*.jpk-force *.jpk-force-map *.jpk-qi-data *.jpk-force.zip *.jpk-force-map.zip *.jpk-qi-data.zip);;
 				Nanoscope files (*.spm *.pfc);;
+				PARK files (*.tiff);;
+
     				PSNEX files (*.tdms )
 
 				"""
@@ -228,7 +230,7 @@ class MainWindow(QtWidgets.QMainWindow):
 			self.remove_all_files_and_results()
 	
 	def getFileList(self, directory):
-		types = ('*.jpk-force', '*.jpk-force-map', '*.jpk-qi-data', '*.jpk-force.zip', '*.jpk-force-map.zip', '*.jpk-qi-data.zip', '*.spm', '*.pfc','*.tdms ')
+		types = ('*.jpk-force', '*.jpk-force-map', '*.jpk-qi-data', '*.jpk-force.zip', '*.jpk-force-map.zip', '*.jpk-qi-data.zip', '*.spm', '*.pfc','*.tdms ','*.tiff')
 		dataset_files = []
 		for files in types:
 			dataset_files.extend(glob.glob(f'{directory}/**/{files}', recursive=True))
